8563ecb092f1e25634b0f35adee2b3c4cd23bc1c
huynhson <danghuynhsonpys@gmail.com>
2018-01-11 18:50:53 +0700

    update code khoi tao fr_CTHDN.cs

--- QuanLyBanHangDienTu/QuanLyBanHangDienTu/Presentation/fr_CTHDN.cs
+++ QuanLyBanHangDienTu/QuanLyBanHangDienTu/Presentation/fr_CTHDN.cs
@@ -39,5 +39,55 @@ namespace QuanLyBanHangDienTu.Presentation
                 sohdn = value;
             }
         }
+        public void setnull()
+        {
+            txtsl.Text = "0";
+            txtdg.Text = "0";
+            txtgg.Text = "0";
+            txttt.Text = "0";
+            cbhang.Text = "";
+        }
+        public void locktext()
+        {
+            txtsl.Enabled = false;
+            txtdg.Enabled = false;
+            txtgg.Enabled = false;
+            cbhang.Enabled = false;
+
+            btmoi.Enabled = true;
+            btluu.Enabled = false;
+            btsua.Enabled = true;
+            btxoa.Enabled = true;
+        }
+        public void un_locktext()
+        {
+            txtsl.Enabled = true;
+            txtdg.Enabled = true;
+            txtgg.Enabled = true;
+            cbhang.Enabled = true;
+
+            btmoi.Enabled = false;
+            btluu.Enabled = true;
+            btsua.Enabled = false;
+            btxoa.Enabled = false;
+        }
+        public void khoitaoluoi()
+        {
+            msds.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            msds.Columns[0].HeaderText = "Số HDN";
+            msds.Columns[0].Frozen = true;
+            msds.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            msds.Columns[0].Width = 100;
+            msds.Columns[1].HeaderText = "Mã Hàng";
+            msds.Columns[1].Width = 100;
+            msds.Columns[2].HeaderText = "Số Lượng";
+            msds.Columns[2].Width = 80;
+            msds.Columns[3].HeaderText = "Đơn Giá";
+            msds.Columns[3].Width = 80;
+            msds.Columns[4].HeaderText = "Giảm Giá";
+            msds.Columns[4].Width = 80;
+            msds.Columns[5].HeaderText = "Thành Tiền";
+            msds.Columns[5].Width = 80;
+        }
     }
 }
