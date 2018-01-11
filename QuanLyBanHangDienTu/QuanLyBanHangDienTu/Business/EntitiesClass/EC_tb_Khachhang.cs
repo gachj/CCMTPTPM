6d2faa481594bf0a50519a66c7c13bbc94fe2749
DuyThanh <duythanh.vo2811@gmail.com>
2018-01-11 11:51:58 +0700

    update

---
+++ QuanLyBanHangDienTu/QuanLyBanHangDienTu/Business/EntitiesClass/EC_tb_Khachhang.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLyBanHangDienTu.Business.EntitiesClass
+{
+    class EC_tb_Khachhang
+    {
+        private string makh;
+        private string tenkh;
+        private string diachi;
+        private string dienthoai;
+
+        public string MAKH
+        {
+            get
+            {
+                return makh;
+            }
+            set
+            {
+                makh = value;
+                if (makh == "")
+                {
+                    throw new Exception("Mã không được bỏ trống");
+                }
+            }
+        }
+        public string TENKH
+        {
+            get
+            {
+                return tenkh;
+            }
+            set
+            {
+                tenkh = value;
+                if (tenkh == "")
+                {
+                    throw new Exception("không được bỏ trống");
+                }
+            }
+        }
+        public string DIACHI
+        {
+            get
+            {
+                return diachi;
+            }
+            set
+            {
+                diachi = value;
+                if (diachi == "")
+                {
+                    throw new Exception("không được bỏ trống");
+                }
+            }
+        }
+        public string DIENTHOAI
+        {
+            get
+            {
+                return dienthoai;
+            }
+            set
+            {
+                dienthoai = value;
+                if (dienthoai == "")
+                {
+                    throw new Exception("không được bỏ trống");
+                }
+            }
+        }
+    }
+}
