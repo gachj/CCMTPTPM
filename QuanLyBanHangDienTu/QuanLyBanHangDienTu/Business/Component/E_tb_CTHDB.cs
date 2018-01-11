3687c54ccf739e9490c2854169a016c3c814792f
DuyThanh <duythanh.vo2811@gmail.com>
2018-01-11 11:48:48 +0700

    update

---
+++ QuanLyBanHangDienTu/QuanLyBanHangDienTu/Business/Component/E_tb_CTHDB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuanLyBanHangDienTu.Business.EntitiesClass;
+using QuanLyBanHangDienTu.DataAccess;
+using System.Windows.Forms;
+
+namespace QuanLyBanHangDienTu.Business.Component
+{
+    class E_tb_CTHDB
+    {
+        SQL_tb_CTHDB cthdbsql = new SQL_tb_CTHDB();
+        public void themoicthdb(EC_tb_CTHDB cthdb)
+        {
+            if (!cthdbsql.kiemtracthdb(cthdb.SOHDB, cthdb.MAHANG))
+            {
+                cthdbsql.themmoicthdb(cthdb);
+            }
+            else
+            {
+                MessageBox.Show("Mã này đã tồn tại,xin chọn Mã khác", "Chú Ý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        public void suacthdb(EC_tb_CTHDB cthdb)
+        {
+            cthdbsql.suacthdb(cthdb);
+        }
+        public void xoacthdb(EC_tb_CTHDB cthdb)
+        {
+            cthdbsql.xoacthdb(cthdb);
+        }
+        //load hóa đơn
+        public void loadmahd(ComboBox cbhd)
+        {
+            cthdbsql.loadmahd(cbhd);
+        }
+        //load hóa đơn
+        public void loadmasp(ComboBox cbsp)
+        {
+            cthdbsql.loadmasp(cbsp);
+        }
+        public string loadtensp(string Tensp, string Masp)
+        {
+            Tensp = cthdbsql.Loadtenhang(Tensp, Masp);
+            return Tensp;
+        }
+
+        public string loaddg(string dg, string Masp)
+        {
+            dg = cthdbsql.Loaddgb(dg, Masp);
+            return dg;
+        }
+    }
+}
---
+++ QuanLyBanHangDienTu/QuanLyBanHangDienTu/Business/EntitiesClass/EC_tb_CTHDB.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLyBanHangDienTu.Business.EntitiesClass
+{
+    class EC_tb_CTHDB
+    {
+        private string sohdb;
+        private string mahang;
+        private string soluong;
+        private string giamgia;
+        private string thanhtien;
+
+        public string SOHDB
+        {
+            get
+            {
+                return sohdb;
+            }
+            set
+            {
+                sohdb = value;
+                if (sohdb == "")
+                {
+                    throw new Exception("Mã không được bỏ trống");
+                }
+            }
+        }
+        public string MAHANG
+        {
+            get
+            {
+                return mahang;
+            }
+            set
+            {
+                mahang = value;
+                if (mahang == "")
+                {
+                    throw new Exception("Mã không được bỏ trống");
+                }
+            }
+        }
+        public string SOLUONG
+        {
+            get
+            {
+                return soluong;
+            }
+            set
+            {
+                soluong = value;
+
+            }
+        }
+        public string GIAMGIA
+        {
+            get
+            {
+                return giamgia;
+            }
+            set
+            {
+                giamgia = value;
+            }
+        }
+        public string THANHTIEN
+        {
+            get
+            {
+                return thanhtien;
+            }
+            set
+            {
+                thanhtien = value;
+            }
+        }
+    }
+}
