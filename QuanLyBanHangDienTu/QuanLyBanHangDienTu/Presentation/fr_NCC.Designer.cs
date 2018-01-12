9420f1d9ab9dd66cd9fb5533b04e97ec9929370a
huynhson <danghuynhsonpys@gmail.com>
2018-01-12 12:43:22 +0700

    form fr_NCC.cs

---
+++ QuanLyBanHangDienTu/QuanLyBanHangDienTu/Presentation/fr_NCC.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace QuanLyBanHangDienTu.Presentation
+{
+    partial class fr_NCC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtdt = new System.Windows.Forms.TextBox();
+            this.txtten = new System.Windows.Forms.TextBox();
+            this.txtdc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtma = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btxoa = new System.Windows.Forms.Button();
+            this.btsua = new System.Windows.Forms.Button();
+            this.btluu = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btmoi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.msds = new System.Windows.Forms.DataGridView();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.msds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtdt
+            // 
+            this.txtdt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtdt.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdt.Location = new System.Drawing.Point(113, 105);
+            this.txtdt.Name = "txtdt";
+            this.txtdt.Size = new System.Drawing.Size(627, 27);
+            this.txtdt.TabIndex = 1;
+            // 
+            // txtten
+            // 
+            this.txtten.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtten.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtten.Location = new System.Drawing.Point(113, 39);
+            this.txtten.Name = "txtten";
+            this.txtten.Size = new System.Drawing.Size(627, 27);
+            this.txtten.TabIndex = 1;
+            // 
+            // txtdc
+            // 
+            this.txtdc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtdc.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdc.Location = new System.Drawing.Point(113, 72);
+            this.txtdc.Name = "txtdc";
+            this.txtdc.Size = new System.Drawing.Size(627, 27);
+            this.txtdc.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Điện Thoại";
+            // 
+            // txtma
+            // 
+            this.txtma.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtma.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtma.Location = new System.Drawing.Point(113, 6);
+            this.txtma.Name = "txtma";
+            this.txtma.Size = new System.Drawing.Size(627, 27);
+            this.txtma.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Địa Chỉ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tên Nhà CC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã Nhà CC";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Turquoise;
+            this.panel3.Controls.Add(this.txtdt);
+            this.panel3.Controls.Add(this.txtten);
+            this.panel3.Controls.Add(this.txtdc);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.txtma);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.ForeColor = System.Drawing.Color.Red;
+            this.panel3.Location = new System.Drawing.Point(0, 265);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(754, 140);
+            this.panel3.TabIndex = 27;
+            // 
+            // btxoa
+            // 
+            this.btxoa.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btxoa.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btxoa.Image = global::QuanLyBanHangDienTu.Properties.Resources._3Xoa;
+            this.btxoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btxoa.Location = new System.Drawing.Point(307, 0);
+            this.btxoa.Name = "btxoa";
+            this.btxoa.Size = new System.Drawing.Size(90, 43);
+            this.btxoa.TabIndex = 3;
+            this.btxoa.Text = "Xóa";
+            this.btxoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btxoa.UseVisualStyleBackColor = true;
+            // 
+            // btsua
+            // 
+            this.btsua.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btsua.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btsua.Image = global::QuanLyBanHangDienTu.Properties.Resources._2Sua;
+            this.btsua.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btsua.Location = new System.Drawing.Point(220, 0);
+            this.btsua.Name = "btsua";
+            this.btsua.Size = new System.Drawing.Size(87, 43);
+            this.btsua.TabIndex = 2;
+            this.btsua.Text = "Sửa";
+            this.btsua.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btsua.UseVisualStyleBackColor = true;
+            // 
+            // btluu
+            // 
+            this.btluu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btluu.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btluu.Image = global::QuanLyBanHangDienTu.Properties.Resources.Luu;
+            this.btluu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btluu.Location = new System.Drawing.Point(129, 0);
+            this.btluu.Name = "btluu";
+            this.btluu.Size = new System.Drawing.Size(91, 43);
+            this.btluu.TabIndex = 1;
+            this.btluu.Text = "Lưu";
+            this.btluu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btluu.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Turquoise;
+            this.panel2.Controls.Add(this.btxoa);
+            this.panel2.Controls.Add(this.btsua);
+            this.panel2.Controls.Add(this.btluu);
+            this.panel2.Controls.Add(this.btmoi);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.ForeColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(0, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(754, 43);
+            this.panel2.TabIndex = 25;
+            // 
+            // btmoi
+            // 
+            this.btmoi.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btmoi.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btmoi.Image = global::QuanLyBanHangDienTu.Properties.Resources._1Them;
+            this.btmoi.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btmoi.Location = new System.Drawing.Point(0, 0);
+            this.btmoi.Name = "btmoi";
+            this.btmoi.Size = new System.Drawing.Size(129, 43);
+            this.btmoi.TabIndex = 0;
+            this.btmoi.Text = "Thêm Mới";
+            this.btmoi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btmoi.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(175, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH MỤC NHÀ CUNG CẤP";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Turquoise;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(754, 50);
+            this.panel1.TabIndex = 24;
+            // 
+            // msds
+            // 
+            this.msds.BackgroundColor = System.Drawing.Color.Turquoise;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.msds.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.msds.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.msds.DefaultCellStyle = dataGridViewCellStyle2;
+            this.msds.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.msds.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(226)))), ((int)(((byte)(238)))));
+            this.msds.Location = new System.Drawing.Point(0, 0);
+            this.msds.Name = "msds";
+            this.msds.Size = new System.Drawing.Size(754, 405);
+            this.msds.TabIndex = 26;
+            // 
+            // fr_NCC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 405);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.msds);
+            this.Name = "fr_NCC";
+            this.Text = "fr_NCC";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.msds)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtdt;
+        private System.Windows.Forms.TextBox txtten;
+        private System.Windows.Forms.TextBox txtdc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtma;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btxoa;
+        private System.Windows.Forms.Button btsua;
+        private System.Windows.Forms.Button btluu;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btmoi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView msds;
+    }
+}
