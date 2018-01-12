935c7469b71d6b774d80911d6a621f954f76a9b6
huynhson <danghuynhsonpys@gmail.com>
2018-01-12 13:26:37 +0700

    code EC_tb_NCC

--- QuanLyBanHangDienTu/QuanLyBanHangDienTu/Business/EntitiesClass/EC_tb_NCC.cs
+++ QuanLyBanHangDienTu/QuanLyBanHangDienTu/Business/EntitiesClass/EC_tb_NCC.cs
@@ -2,11 +2,76 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace QuanLyBanHangDienTu.Business.EntitiesClass
 {
     class EC_tb_NCC
     {
+        private string mancc;
+        private string tenncc;
+        private string diachi;
+        private string dienthoai;
+
+        public string MANCC
+        {
+            get
+            {
+                return mancc;
+            }
+            set
+            {
+                mancc = value;
+                if (mancc == "")
+                {
+                    throw new Exception("Mã không được bỏ trống");
+                }
+            }
+        }
+        public string TENNCC
+        {
+            get
+            {
+                return tenncc;
+            }
+            set
+            {
+                tenncc = value;
+                if (tenncc == "")
+                {
+                    throw new Exception("không được bỏ trống");
+                }
+            }
+        }
+        public string DIACHI
+        {
+            get
+            {
+                return diachi;
+            }
+            set
+            {
+                diachi = value;
+                if (diachi == "")
+                {
+                    throw new Exception("không được bỏ trống");
+                }
+            }
+        }
+        public string DIENTHOAI
+        {
+            get
+            {
+                return dienthoai;
+            }
+            set
+            {
+                dienthoai = value;
+                if (dienthoai == "")
+                {
+                    throw new Exception("không được bỏ trống");
+                }
+            }
+        }
     }
 }
