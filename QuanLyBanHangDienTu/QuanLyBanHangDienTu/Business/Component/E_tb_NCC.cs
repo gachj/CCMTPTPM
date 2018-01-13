18a1ce1b60f760104ef2f8d48d770b95710ad9b0
huynhson <danghuynhsonpys@gmail.com>
2018-01-13 09:20:04 +0700

    code E_tb_NCC

--- QuanLyBanHangDienTu/QuanLyBanHangDienTu/Business/Component/E_tb_NCC.cs
+++ QuanLyBanHangDienTu/QuanLyBanHangDienTu/Business/Component/E_tb_NCC.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace QuanLyBanHangDienTu.Business.Component
 {
     class E_tb_NCC
     {
-
+        SQL_tb_NCC nccsql = new SQL_tb_NCC();
+        public void themoincc(EC_tb_NCC ncc)
+        {
+            if (!nccsql.kiemtrancc(ncc.MANCC))
+            {
+                nccsql.themmoincc(ncc);
+            }
+            else
+            {
+                MessageBox.Show("Mã này đã tồn tại,xin chọn Mã khác", "Chú Ý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        public void suancc(EC_tb_NCC ncc)
+        {
+            nccsql.suancc(ncc);
+        }
+        public void xoancc(EC_tb_NCC ncc)
+        {
+            nccsql.xoancc(ncc);
+        }
     }
 }
