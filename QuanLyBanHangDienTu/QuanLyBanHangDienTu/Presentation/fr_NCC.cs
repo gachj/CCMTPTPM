62a2099d73fe64b331069c6d1306a7b624cff03a
huynhson <danghuynhsonpys@gmail.com>
2018-01-13 09:20:48 +0700

    code fr_NCC.cs

--- QuanLyBanHangDienTu/QuanLyBanHangDienTu/Presentation/fr_NCC.cs
+++ QuanLyBanHangDienTu/QuanLyBanHangDienTu/Presentation/fr_NCC.cs
@@ -5,8 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using QuanLyBanHangDienTu.Business.Component;
+using QuanLyBanHangDienTu.Business.EntitiesClass;
+using QuanLyBanHangDienTu.DataAccess;
 
 namespace QuanLyBanHangDienTu.Presentation
 {
@@ -15,6 +18,183 @@ namespace QuanLyBanHangDienTu.Presentation
         public fr_NCC()
         {
             InitializeComponent();
+        }
+        E_tb_NCC thucthi = new E_tb_NCC();
+        ConnectDB cn = new ConnectDB();
+        EC_tb_NCC ck = new EC_tb_NCC();
+        bool themmoi;
+        int dong = 0;
+
+        public void setnull()
+        {
+            txtma.Text = "";
+            txtten.Text = "";
+            txtdt.Text = "";
+            txtdc.Text = "";
+        }
+        public void locktext()
+        {
+            txtma.Enabled = false;
+            txtten.Enabled = false;
+            txtdc.Enabled = false;
+            txtdt.Enabled = false;
+
+            btmoi.Enabled = true;
+            btluu.Enabled = false;
+            btsua.Enabled = true;
+            btxoa.Enabled = true;
+        }
+        public void un_locktext()
+        {
+            txtma.Enabled = true;
+            txtten.Enabled = true;
+            txtdc.Enabled = true;
+            txtdt.Enabled = true;
+
+            btmoi.Enabled = false;
+            btluu.Enabled = true;
+            btsua.Enabled = false;
+            btxoa.Enabled = false;
+        }
+        public void khoitaoluoi()
+        {
+            msds.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            msds.Columns[0].HeaderText = "Mã NCC";
+            msds.Columns[0].Frozen = true;
+            msds.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            msds.Columns[0].Width = 100;
+            msds.Columns[1].HeaderText = "Tên NCC";
+            msds.Columns[1].Width = 300;
+            msds.Columns[2].HeaderText = "Địa Chỉ";
+            msds.Columns[2].Width = 300;
+            msds.Columns[3].HeaderText = "Điện Thoại";
+            msds.Columns[3].Width = 300;
+
+        }
+        public void hienthi()
+        {
+            string sql = "SELECT     mancc, tenncc, diachi, dienthoai FROM tb_NCC";
+            msds.DataSource = cn.taobang(sql);
+            SqlConnection con = cn.getcon();
+            con.Open();
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        private void btmoi_Click(object sender, EventArgs e)
+        {
+            themmoi = true;
+            un_locktext();
+            setnull();
+            txtma.Enabled = true;
+            txtma.Focus();
+        }
+
+        private void btluu_Click(object sender, EventArgs e)
+        {
+            if (txtma.Text != "")
+            {
+                if (themmoi == true)
+                {
+                    try
+                    {
+                        ck.MANCC = txtma.Text;
+                        ck.TENNCC = txtten.Text;
+                        ck.DIACHI = txtdc.Text;
+                        ck.DIENTHOAI = txtdt.Text;
+
+                        thucthi.themoincc(ck);
+                        locktext();
+                        hienthi();
+                        MessageBox.Show("Đã Lưu Thành Công", "Chú Ý", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "Chú Ý", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                    try
+                    {
+                        ck.MANCC = txtma.Text;
+                        ck.TENNCC = txtten.Text;
+                        ck.DIACHI = txtdc.Text;
+                        ck.DIENTHOAI = txtdt.Text;
+
+                        thucthi.suancc(ck);
+                        MessageBox.Show("Đã Sửa Thành Công Thành Công", "Chú Ý", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "Chú Ý", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                txtma.Enabled = true;
+                locktext();
+                hienthi();
+            }
+            else
+            {
+                MessageBox.Show("Mã Không được để trống", "Chú Ý", MessageBoxButtons.OK);
+                txtma.Focus();
+            }
+        }
+
+        private void btsua_Click(object sender, EventArgs e)
+        {
+            themmoi = false;
+            un_locktext();
+            txtma.Enabled = false;
+            txtten.Focus();
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Xóa dữ liệu này?", "Chú Ý", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                try
+                {
+                    ck.MANCC = txtma.Text;
+
+                    thucthi.xoancc(ck);
+                    MessageBox.Show("Đã Xóa Thành Công", "Chú Ý", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    hienthi();
+                    setnull();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Lỗi");
+                }
+            }
+        }
+        private void fr_NCC_Load(object sender, EventArgs e)
+        {
+            locktext();
+            hienthi();
+            khoitaoluoi();
+        }
+
+        private void msds_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dong = e.RowIndex;
+            txtma.Text = msds.Rows[dong].Cells[0].Value.ToString();
+            txtten.Text = msds.Rows[dong].Cells[1].Value.ToString();
+            txtdc.Text = msds.Rows[dong].Cells[2].Value.ToString();
+            txtdt.Text = msds.Rows[dong].Cells[3].Value.ToString();
+            locktext();
+        }
+
+        private void txtdt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
         }
     }
 }
